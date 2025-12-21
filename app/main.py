deffb9f8168d0d1a302a1b0ff94eac01d4e24716
Daniskin <Danis_90@mail.ru>
2025-12-21 16:07:01 +0500

    fix: update API router import to use plane routes

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from app.core.models.db_helper import db_helper
 from app.core.config import settings
 from app.core.paths import STATIC_DIR
 
-from app.api import router as api_router
+from app.routers.plane import router as api_plane
 from app.routers.web.dashboard import router as dashboard_router
 
 
@@ -20,7 +20,7 @@ async def lifespan(app: FastAPI):
 
 main_app = FastAPI(lifespan=lifespan)
 main_app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
-main_app.include_router(api_router,
+main_app.include_router(api_plane,
                         prefix=settings.api.prefix)
 main_app.include_router(dashboard_router)
 
